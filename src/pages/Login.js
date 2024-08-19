6679888bfd0ae323cc4b184b2317d8c976d84d73
tanu-rathore <tanu.rtr14@gmail.com>
2024-08-19 19:09:44 +0530

    updated

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,65 +1,77 @@
+import React, { useState } from "react";
+import Button from "../components/Button";
+import { useLocation, useNavigate } from "react-router-dom";
+import "./styles/Login.css";
 
-import React, { useState } from 'react';
-import Button from '../components/Button';
-import { useLocation, useNavigate } from 'react-router-dom';
-import './styles/Login.css';
 
 function Login() {
   const location = useLocation();
-  const { userType } = location.state; 
+  const { userType } = location.state;
 
-  const [email, setEmail] = useState('');
-  const [phoneNo, setPhoneNo] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [phoneNo, setPhoneNo] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   let template;
-  if(userType === 'Admin'){
-    template = <input 
-    type='email' 
-    placeholder='Enter your email' 
-    value={email} 
-    onChange={(e) => setEmail(e.target.value)} 
-    required
-  />
+  if (userType === "Admin") {
+    template = (
+      <input
+        type="email"
+        placeholder="Enter your email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+    );
   } else {
-    template = <input 
-    type='number' 
-    placeholder='Enter your phone number' 
-    value={phoneNo} 
-    onChange={(e) => setPhoneNo(e.target.value)} 
-    required
-  />
+    template = (
+      <input
+        type="tel"
+        placeholder="Enter your phone number"
+        value={phoneNo}
+        onChange={(e) => setPhoneNo(e.target.value)}
+        required
+      />
+    );
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Email:', email);
-    console.log('Password:', password);
-    console.log('Phone number:', phoneNo);
+    console.log("Email:", email);
+    console.log("Password:", password);
+    console.log("Phone number:", phoneNo);
     // post api
     navigate("/adminDashboard");
   };
 
-  return (
-    <div className="login-container">
-      <div className="login-form-container">
-        <h2 className="login-title">{`Login as ${userType}`}</h2>
-        <form onSubmit={handleSubmit} className="login-form">
-          <div className="input-container">
-            {template}
-          </div>
-          <div className="input-container">
-            <input 
-              type='password' 
-              placeholder='Enter your password' 
-              value={password} 
-              onChange={(e) => setPassword(e.target.value)} 
-              required
+  return(
+    <div className="siginin-container">
+      <div className="signin-content">
+        <div className="signin-form">
+          <h2>{`Welcome ${userType}!`}</h2>
+          <p>Sign in to Continue</p>
+          <form>
+            <div className="signin-input-field">
+              {template}
+            </div>
+            <div className="signin-input-field">
+              <input
+                type="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <Button
+              name="Sign-in"
+              className="Signin-button"
+              onClick={(e) => handleSubmit(e)}
             />
-          </div>
-          <Button name="Login" className="login-button" />
-        </form>
+          </form>
+        </div>
+        <div className="signin-image"></div>
       </div>
     </div>
   );
